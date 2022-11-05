88b44a76effc576a7c0bc14778c1949f4cf99e09
yayayacc <1453991938@qq.com>
2022-11-05 22:24:02 +0800

    2.2

--- main.c
+++ main.c
@@ -6,14 +6,15 @@
 int main()
 {
      char choice;      pid=0;
-    free_block = init_free_block(mem_size); //初始化空闲区
+    free_block = init_free_block(mem_size); //初始化空闲区/*初始化空闲块，默认为一块，可以指定大小及起始地址*/
+
     while(1) {
         display_menu();	//显示菜单
         fflush(stdin);
         choice=getchar();	//获取用户输入
         switch(choice){
-            case '1': set_mem_size(); break; 	//设置内存大小
-            case '2': set_algorithm();flag=1; break;//设置算法
+            case '1': set_mem_size(); break; 	//设置内存大小,仅改变了内存大小，起始地点没改变
+            case '2': set_algorithm();flag=1; break;//设置算法，并且用算法重排空闲区链表
             case '3': new_process(); flag=1; break;//创建新进程
             case '4': kill_process(); flag=1;   break;//删除进程
             case '5': display_mem_usage();    flag=1; break;	//显示内存使用
